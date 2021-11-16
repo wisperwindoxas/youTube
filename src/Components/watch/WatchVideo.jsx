39c13f01a2a13211cbc1625ec2bf807c1038b100
wisperwindoxas <windoxas@gmail.com>
2021-11-16 17:13:09 +0500

    add settings

--- src/Components/watch/WatchVideo.jsx
+++ src/Components/watch/WatchVideo.jsx
@@ -1,44 +1,77 @@
-import React from 'react'
-import './watchVideo.css'
-import { FaPlay, FaPause} from "react-icons/fa"; 
-import {volumeUp, volumeMiddle, volumeLow, mute, nextBtn} from './watchIconsVideo'
+import React from "react";
+import "./watchVideo.css";
+import { FaPlay, FaPause } from "react-icons/fa";
+import {
+  volumeUp,
+  volumeMiddle,
+  volumeLow,
+  mute,
+  nextBtn,
+  setting,
+  small,
+  middle,
+  full,
+  middle_two
+} from "./watchIconsVideo";
 
 export default function WatchVideo() {
+  const [isPlaying, setIsplaying] = React.useState(false);
+  const [isMiddle, setIsMiddle] = React.useState(false);
 
-    const [isPlaying, setIsplaying] =React.useState(false)
-   
-    return (
-        <div className="watch">
+  return (
+    <div className="watch">
+      <div className="videoPlayer">
+        <video
+          width="100%"
+          id="video"
+          autoPlay="true"
+          src="/video/LITTLE BIG – SKIBIDI (official music video).mp4"
+          poster="/video/Enrique Iglesias.jpg"
+        ></video>
+        <div className="controll">
+          <div className="progressbar">
+            <div className="progress"></div>
+          </div>
+          <div className="controlsBtn">
+            <div className="play_next_volume_time">
+              <button onClick={() => setIsplaying(!isPlaying)} className="play">
+                {isPlaying ? <FaPause /> : <FaPlay />}
+              </button>
+              <button className="next">
+                <img src={nextBtn} alt="" />
+              </button>
+              <div className="volume">
+                <img src={volumeUp} alt="" />
+                <input
+                  type="range"
+                  id="volume"
+                  name="volume"
+                  min="0"
+                  max="11"
+                />
+              </div>
+              <div className="time">
+                <span>1:48</span> / <span>24:05</span>
+              </div>
+            </div>
 
-               <div className="videoPlayer">
-                <video width="100%" id="video" autoPlay="true" src="/video/LITTLE BIG – SKIBIDI (official music video).mp4" poster="/video/Enrique Iglesias.jpg"></video>
-                   <div className="controll">
-                        <div className="progressbar">
-                            <div className="progress"></div>
-                        </div>
-                        <div className="controlsBtn">
-                            <div className="play_next_volume_time">
-                                <button onClick={() => setIsplaying(!isPlaying) } className="play">
-                                    {isPlaying ?<FaPause/>  : <FaPlay/>}
-                                   
-                                </button>
-                                <button className="next">
-                                       <img src={nextBtn} alt="" />
-                                </button>
-                                <div className="volume">
-                                    <img src={volumeUp} alt="" />
-                                    <input type="range" id="volume" name="volume" min="0" max="11"/>
-                                </div>
-                                <div className="time">
-                                    <span>1:48</span> / <span>24:05</span>
-                                </div>
-                            </div>
-                        </div>
-                   </div>
-               </div>
-            
+            <div className="setting_full_screen">
+              <button className="setting">
+                <img src={setting} alt="" />
+              </button>
+              <button className="small">
+                <img src={small} alt="" />
+              </button>
+              <button className="middle">
+                <img onClick={() => setIsMiddle(!isMiddle)} src={isMiddle ? middle: middle_two} alt="" />
+              </button>
+              <button className="full">
+                <img src={full} alt="" />
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
-
--- src/Components/watch/watchIconsVideo.jsx
+++ src/Components/watch/watchIconsVideo.jsx
@@ -1,7 +1,23 @@
-import volumeUp from './icons/volume-up.png'
-import volumeMiddle from './icons/volumeMiddle.png'
-import volumeLow from './icons/volume-low.png'
-import mute from './icons/mute.png'
-import nextBtn from './icons/next.png'
+import volumeUp from "./icons/volume-up.png";
+import volumeMiddle from "./icons/volumeMiddle.png";
+import volumeLow from "./icons/volume-low.png";
+import mute from "./icons/mute.png";
+import nextBtn from "./icons/next.png";
+import setting from "./icons/settings.png";
+import small from "./icons/small_screen.svg";
+import middle from "./icons/middle_screen.svg";
+import middle_two from "./icons/middle_two_screen.svg";
+import full from "./icons/full_screen.png";
 
-export {volumeUp, volumeMiddle, volumeLow, mute, nextBtn}
+export {
+  volumeUp,
+  volumeMiddle,
+  volumeLow,
+  mute,
+  nextBtn,
+  setting,
+  small,
+  middle,
+  full,
+  middle_two,
+};
